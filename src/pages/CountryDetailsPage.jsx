3690adb819b8e61b94bfa7879d4b6d4e0ab18acd
Alejandro <alegaralonrgmail.com>
2023-07-30 12:45:25 +0200

    done2

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -7,8 +7,7 @@ import { } from 'react-router-dom'
 function CountryDetails() {
     const params = useParams()
     const [country, setCountry] = useState({})
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [isLoading, setIsLoading] = useState(true)
+   
     
     console.log(params.countryId)
     
